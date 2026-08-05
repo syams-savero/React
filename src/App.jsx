9c77610431f818e75924e8ac0ef507c1b62d5ac5
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-06 00:06:21 +0700

    add: 3 row button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 export default function Square() {
   return (
     <>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
-      <button className="Square">X</button>
+      <div className="board-row">
+      <button className="Square">1</button>
+      <button className="Square">2</button>
+      <button className="Square">3</button>
+      </div>
+      
+      <div className="board-row">
+      <button className="Square">4</button>
+      <button className="Square">5</button>
+      <button className="Square">6</button>
+      </div>
+
+      <div className="board-row">
+      <button className="Square">7</button>
+      <button className="Square">8</button>
+      <button className="Square">9</button>
+      </div>
     </>
   )
 }
