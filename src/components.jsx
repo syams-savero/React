28301dadc5aefc854eabc438ca1903b1ee247df8
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-10 23:43:11 +0700

    create components.tsx

---
+++ src/components.jsx
@@ -0,0 +1,14 @@
+function Tes() {
+  return (
+    <h2>Buat Garis untuk menang!</h2>
+  )
+}
+
+export default function Percobaan() {
+  return (
+    <div>
+      <h1>Syarat menang TicTacToe</h1>
+      <Tes />
+    </div>
+  )
+}
