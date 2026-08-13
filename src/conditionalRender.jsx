177dd4c0c2afe69936abb35d6a6340d29ce595b1
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-13 23:03:19 +0700

    add: conditional function with ternary

--- src/conditionalRender.jsx
+++ src/conditionalRender.jsx
@@ -11,3 +11,22 @@ function Item({ name, isPacked}) {
     </li>
   );
 }
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>List barang</h1>
+      <ul>
+        <Item 
+          isPacked={true}
+          name="Sandal"
+        />
+
+        <Item 
+          isPacked={false}
+          name="Sepatu"
+        />
+      </ul>
+    </section>
+  )
+}
