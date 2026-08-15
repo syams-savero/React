8cf96143321289b138d31e0c0cd2f0104b24554d
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-15 22:15:06 +0700

    add: array for filter

---
+++ src/renderList.jsx
@@ -0,0 +1,34 @@
+const people = [
+  'Gatot: Judge',
+  'Nasir: Dancer',
+  'King: Runner',
+  'Rusdy: Barber'
+];
+
+export default function List() {
+  const peopleJob = people.map(person => <li>{person}</li>);
+  return <ul style={{ textAlign: 'left' }}>{peopleJob}</ul>
+}
+
+
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
