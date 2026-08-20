22473ba61af53c189d7a1c92898f74b200a2c561
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-20 22:55:02 +0700

    add: button input

--- src/taskManager.jsx
+++ src/taskManager.jsx
@@ -6,10 +6,18 @@ export default function TaskManager() {
     { id: 2, text: 'Menyimpan perubahan di github', done: false },
   ]);
   const [text, setText] = useState('');
+
+  function handleAdd() {
+    if (text.trim() === '') return;
+    setTask([...task, { id: Date.now(), text: text.trim(), done: false}]);
+    setText('');
+  }
+
   return (
   <div>
     <h2>Mini Task Manager</h2>
     <input type="text" placeholder="tambah tugas baru" value={text} onChange={(e) => setText(e.target.value)}/>
+    <button onClick={handleAdd}>Tambah</button>
   </div>
   );
 }
