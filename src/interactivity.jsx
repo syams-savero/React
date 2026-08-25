c8f8df1789bb3bd1bffb2eab88cf8c2a62cd4600
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-25 21:41:29 +0700

    add: reusable alert button

--- src/interactivity.jsx
+++ src/interactivity.jsx
@@ -1,9 +1,19 @@
+function AlertButton({ message, buttonName }) {
+  return (
+  <button onClick={() => alert(message)}>{buttonName}</button>
+  );
+}
+
 export default function Button() {
   function click() {
     alert('clicked');
   }
-
+  
   return (
+  <div>
   <button onClick={click}>Clicked</button>
+  <AlertButton message="Alert 1 works" buttonName="Alert 1" />
+  <AlertButton message="Alert 2 works" buttonName="Alert 2" />
+  </div>
   )
 }
