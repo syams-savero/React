918b28d1063bf4c2d72bdc6ab8dfdf98c39c8c9b
syams-savero <muhammadsyamssavero@gmail.com>
2026-08-26 21:04:28 +0700

    add: file and 9 div

---
+++ src/reactionGame.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function ReactionGame() {
+  return (
+  <>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  </>
+  )
+}
