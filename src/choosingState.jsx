436a59e59b946177e93f42a12ef56377a7da1227
syams-savero <muhammadsyamssavero@gmail.com>
2026-09-05 23:19:10 +0700

    add: group related state

---
+++ src/choosingState.jsx
@@ -0,0 +1,25 @@
+import ( useState ) from 'react';
+
+export default function MovingDot() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  return (
+  <div
+    onPointerMove={(e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }}
+      style={{ position: 'relative', width: '100vw', height: '50vh', border: '1px solid black' }}>
+
+  <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }} />
+    </div>
+  )
+}
