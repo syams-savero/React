22a55ed7fed1e96692407e4174384a44e652e278
syams-savero <muhammadsyamssavero@gmail.com>
2026-09-06 23:00:52 +0700

    add form

--- src/avoidContradictionState.jsx
+++ src/avoidContradictionState.jsx
@@ -10,4 +10,33 @@ export default function FeedbackForm() {
   await sendMessage(text);
   setStatus('sent');
   }
+
+  const isSending = status === 'sending';
+  const isSent = status === 'sent';
+
+  if (isSent) {
+    return <h1>Thanks for your feedback!</h1>;
+  }
+
+  return (
+  <form onSubmit={handleSubmit} style={{ textAlign: 'left'}}>
+  <h3>Tolong masukan saran dan pendapat anda :)</h3>
+  <textArea 
+    disabled={isSending}
+    value={text}
+    onChange={e => setText(e.target.value)}
+    />
+  
+  <br />
+  <button disabled={isSending} tpye="isSubmit">Submit</button>
+  {isSending && <p>sending...</p>}
+  </form>
+  );
 }
+
+function sendMessage(text) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  });
+}
+
