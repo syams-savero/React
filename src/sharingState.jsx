3289ee078124607c778892f6e9bbeb21a2be229d
syams-savero <muhammadsyamssavero@gmail.com>
2026-09-10 15:35:20 +0700

    Add: Calling 2 shared useState function

--- src/sharingState.jsx
+++ src/sharingState.jsx
@@ -15,4 +15,24 @@ function Panel({ title, description, isActive, onShow }) {
   )
 }
 
+export default function Animal() {
+  const [active, setActive] = useState(0);
+  return (
+  <>
+    <h2>Cat</h2>
+    <Panel
+      title='About'
+      description='a small, carnivorous mammal and one of the most popular companion animals in the world'
+      isActive={active === 1}
+      onShow={() => setActive(1)}
+    />
 
+    <Panel
+      title='Fun Fact'
+      description='Cats spend roughly 70% of their lives sleeping and possess 32 muscles in each ear, allowing them to swivel 180 degrees'
+      isActive={active === 2}
+      onShow={() => setActive(2)}
+    />
+  </>
+  )
+}
