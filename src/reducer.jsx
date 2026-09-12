d579f6e37ad40dc4dc5d196cf72dba22ae916dc2
syams-savero <muhammadsyamssavero@gmail.com>
2026-09-12 23:20:20 +0700

    add: fix onClick typo

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useReducer } from 'react';
 
+/*
 export default function Counter() {
   const [count, setCount] = useState(0);
 
@@ -22,5 +24,50 @@ export default function Counter() {
     <button onClick={handleDecrement}>-1</button>
     <button onClick={handleReset}>reset</button>
   </>
+  )
+} */
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'incremented': {
+      return state + 1;
+    }
+
+    case 'decremented': {
+      return state - 1;
+    }
+
+    case 'reset': {
+      return 0;
+    }
+
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
+
+export default function Counter() {
+  const [count, dispatch] = useReducer(countReducer, 0);
+
+  function handleIncrement() {
+    dispatch({ type: 'incremented' });
+  }
+
+  function handleDecrement() {
+    dispatch({ type: 'decremented' });
+  }
+
+  function reset() {
+    dispatch({ type: 'reset' });
+  }
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}>+1</button>
+      <button onClick={handleDecrement}>-1</button>
+      <button onClick={reset}>reset</button>
+    </>
   )
 }
